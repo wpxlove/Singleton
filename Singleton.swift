6db81ab548ff198e15b04dd753e99802c8697456
zack2012 <zbhne@163.com>
2014-10-15 08:13:52 +0800

    v1.00

---
+++ Singleton.swift
@@ -0,0 +1,13 @@
+//
+//  Singleton.swift
+//  SwiftSingleton
+//
+//  Created by shiyueli7 on 14-10-14.
+//  Copyright (c) 2014年 shiyueli7. All rights reserved.
+//
+
+import UIKit
+
+class Singleton:  {
+   
+}
